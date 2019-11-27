aec26cc7fc3787643ad8e183858f8ee805f7c191
Ketan panwar <43447711+KetanPanwar@users.noreply.github.com>
2019-11-28 00:29:55 +0530

    Update DASK_19.py

--- DASK_19.py
+++ DASK_19.py
@@ -585,4 +585,25 @@ plt.show()
 
 
 
-  
+  # Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 4, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clusters
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 80, c = 'red', label = 'Cluster 1')
+# x-coordinate = X[y_kmeans == 0,0] 2nd 0 for the 0th column , y_kmeans==0 for Cluster 1
+# y-coordinate = X[y_kmeans == 0,1]
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 80, c = 'blue', label = 'Cluster 2')
+
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+'''
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+'''
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of Players')
+plt.legend()
+plt.show()
+
+
