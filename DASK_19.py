8578b7063509c61c5d9efdd7a6e56543b06638da
Ketan panwar <43447711+KetanPanwar@users.noreply.github.com>
2019-11-28 00:28:53 +0530

    Update DASK_19.py

--- DASK_19.py
+++ DASK_19.py
@@ -503,4 +503,86 @@ for p , n , c in zip(posi , player , club_l):
     if p in defenders:
         print('{} [Club : {} , Position : {} , Age : {}]'.format(n , c , p , 
                                                                 dataset['Age'][dataset['Name'] == n].values[0]))
+        
+# # Clustering Players based on K-Means Clustering and Elbow Method:
+
+# # Performing K Means Clustering on Players Based on Age , Overall and Potential
+
+# In[36]:
+
+
+X = dataset.iloc[:,[1,3,4]].values
+
+
+# Finding the Ideal Number of Clusters using Elbow Method
+
+# In[37]:
+
+
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_) #kmeans.inertia_ finds Sum of squared distances of samples to their closest cluster center.
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+# In[38]:
+
+
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 2, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clusters
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 50, c = 'red', label = 'Cluster 1')
+# x-coordinate = X[y_kmeans == 0,0] 2nd 0 for the 0th column , y_kmeans==0 for Cluster 1
+# y-coordinate = X[y_kmeans == 0,1]
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 50, c = 'blue', label = 'Cluster 2')
+'''
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+'''
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+
+# # Performing K Means Clustering on Players Based on their Playing Features
+
+# In[39]:
+
+
+X = dataset.iloc[:,15:].values
+
+
+# 
+# Using the elbow method to find the optimal number of clusters
+
+# In[40]:
+
+
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_) #kmeans.inertia_ finds Sum of squared distances of samples to their closest cluster center.
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+
   
